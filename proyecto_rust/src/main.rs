b306e02f7ec4c414d7413699272bfeb675e9dcbe
Damiankuc <damiancaminos2017@gmail.com>
2026-03-24 13:09:18 -0300

    Agregado de operaciones

--- proyecto_rust/src/main.rs
+++ proyecto_rust/src/main.rs
@@ -1,5 +1,8 @@
 fn main() {
     let x = 5;
     let y = 10;
-    println!("El valor obtnido de x+y es {}", x + y);
+    println!("El valor obtenido de x+y es {}", x + y);
+    println!("El valor obtenido de x-y es {}", x - y);
+    println!("El valor obtenido de x*y es {}", x * y);
+    println!("El valor obtenido de x/y es {}", x / y);
 }
