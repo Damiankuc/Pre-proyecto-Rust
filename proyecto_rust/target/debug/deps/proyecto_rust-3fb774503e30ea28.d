1b65f93563c45b069c686e49d4bc6aefb5a5dd06
Damiankuc <damiancaminos2017@gmail.com>
2026-03-25 18:57:40 -0300

    cambios varios

---
+++ proyecto_rust/target/debug/deps/proyecto_rust-3fb774503e30ea28.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\Rust Forever\proyecto_rust\target\debug\deps\proyecto_rust-3fb774503e30ea28.d: src\main.rs
+
+C:\Users\HP\Desktop\Rust Forever\proyecto_rust\target\debug\deps\libproyecto_rust-3fb774503e30ea28.rmeta: src\main.rs
+
+src\main.rs:
---
+++ proyecto_rust/target/debug/deps/proyecto_rust-ecc7b51e6de3fd55.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\Rust Forever\proyecto_rust\target\debug\deps\proyecto_rust-ecc7b51e6de3fd55.d: src\main.rs
+
+C:\Users\HP\Desktop\Rust Forever\proyecto_rust\target\debug\deps\libproyecto_rust-ecc7b51e6de3fd55.rmeta: src\main.rs
+
+src\main.rs:
