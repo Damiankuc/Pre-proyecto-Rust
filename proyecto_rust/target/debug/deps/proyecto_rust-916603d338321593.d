368f19f340a48cd6ec23e7d89997b94be4e59599
Damiankuc <damiancaminos2017@gmail.com>
2026-03-27 22:13:08 -0300

    Test realizados

---
+++ proyecto_rust/target/debug/deps/proyecto_rust-916603d338321593.d
@@ -0,0 +1,5 @@
+C:\Users\HP\Desktop\Rust Forever\proyecto_rust\target\debug\deps\proyecto_rust-916603d338321593.d: src\main.rs
+
+C:\Users\HP\Desktop\Rust Forever\proyecto_rust\target\debug\deps\proyecto_rust-916603d338321593.exe: src\main.rs
+
+src\main.rs:
